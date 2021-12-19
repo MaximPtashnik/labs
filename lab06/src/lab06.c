930829a9f9e05e9e722b1a7601b141f51dbf14c3
Максим Пташник <90497308+MaximPtashnik@users.noreply.github.com>
2021-12-19 22:36:08 +0200

    Add files via upload

---
+++ lab06/src/lab06.c
@@ -0,0 +1,14 @@
+//lab06 task 5
+
+
+
+int main()
+{
+	int a[10] = {5, 10, 3, 65, 96, 23, 43, 92, 100, 58}, i, result = 0;
+	for (i = 0; i < 10; i++)
+	{
+		result = result + a[i];
+	}
+	result = result/10;
+	return 0;
+}
