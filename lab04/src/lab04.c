35aafbbd91e41edb365b7e20c7e9a3f6c1c31a55
Максим Пташник <90497308+MaximPtashnik@users.noreply.github.com>
2021-12-19 22:31:26 +0200

    Add files via upload

---
+++ lab04/src/lab04.c
@@ -0,0 +1,18 @@
+//task 1 
+
+int main()
+{
+    int year = 2000;
+    int century;
+
+    if (year % 100 != 0)
+  
+        century = year / 100 + 1;
+    
+    else
+    
+        century = year / 100;
+    
+
+    return 0;
+}
